8a631d5f01c1640c5e1681cae33f7f55023f9513
Hurin Hu <hurin@live.ca>
2025-04-23 14:45:46 +1200

    Added longer delay time to make sure data rendered

--- addon/models/tracker-model.js
+++ addon/models/tracker-model.js
@@ -8,7 +8,7 @@ export default class TrackerModel extends Model.extend(RelationshipTrackerMixin)
   async init() {
     super.init(...arguments);
     // delay to avoid relationship recursive loop
-    await this.delay(200);
+    await this.delay(300);
     this.saveInitialState();
   }
 
